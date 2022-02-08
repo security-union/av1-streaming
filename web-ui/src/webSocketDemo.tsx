07cedaa2c490f111376bb960aeeb1460e5e8d11a
Dario Lencina Talarico <dario@armore.dev>
2022-02-08 09:07:58 -0600

    save

---
+++ web-ui/src/webSocketDemo.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+export const WebSocketDemo = () => {
+  //Public API that will echo messages sent to it back to the client
+  const [socketUrl, setSocketUrl] = useState('wss://echo.websocket.org');
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const {
+    sendMessage,
+    lastMessage,
+    readyState,
+  } = useWebSocket(socketUrl);
+
+  useEffect(() => {
+    if (typeof lastMessage === 'object') {
+    setMessageHistory(prev => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const handleClickChangeSocketUrl = useCallback(() =>
+    setSocketUrl('wss://demos.kaazing.com/echo'), []);
+
+  const handleClickSendMessage = useCallback(() =>
+    sendMessage('Hello'), []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return (
+    <div>
+      <button
+        onClick={handleClickChangeSocketUrl}
+      >
+        Click Me to change Socket Url
+      </button>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 'Hello'
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory
+          .map((message, idx) => <span key={idx}>{message ? message.data : null}</span>)}
+      </ul>
+    </div>
+  );
+};
