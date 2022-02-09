c3b58abb79005840c2c4d241863ef3e5e33842ae
Dario Lencina Talarico <dario@armore.dev>
2022-02-09 01:55:16 -0600

    save

--- web-ui/src/webSocketDemo.tsx
+++ web-ui/src/webSocketDemo.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useCallback, useEffect, Dispatch, SetStateAction } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
-
+import Webcam from 'react-webcam';
 const webSocketURL = 'ws://localhost:10000';
+let codec_string = "av1";
+
 export const WebSocketDemo = () => {
   //Public API that will echo messages sent to it back to the client
   const [socketUrl, setSocketUrl] = useState(webSocketURL);
   const [messageHistory, setMessageHistory]: [any[], Dispatch<SetStateAction<any[]>>] = useState([{ data: "sdfsdf"}]);
+  const webcamRef = React.useRef(null);
 
   const {
     sendMessage,
@@ -33,8 +36,76 @@ export const WebSocketDemo = () => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
+  const [capturing, setCapturing] = React.useState(false);
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    async function captureAndEncode(processChunk: any) {
+      let frame_counter = 0;
+      var track = webcamRef.current.stream.getTracks()[0];
+      var settings = track.getSettings();
+      var pending_outputs = 0;
+      var prc = new MediaStreamTrackProcessor(track);
+      var frameStream = prc.readable;
+      const frameReader= frameStream.getReader();
+    
+      const init = {
+        output: (chunk) => {
+          pending_outputs--;
+          processChunk(chunk);
+        },
+        error: (e: Error) => {
+          console.error(e.message);
+        }
+      };
+    
+      const config = {
+        codec: codec_string,
+        width: settings.width,
+        height: settings.height,
+        bitrate: 10e6,
+      };
+    
+      let encoder = new VideoEncoder(init);
+      encoder.configure(config);
+      frameReader.read().then(function processFrame({done, value}) { 
+        if(done||stop) {
+          value.close();
+          encoder.close();
+          return;
+        }
+    
+        if (!stop && pending_outputs <= 30) {
+          if (++frame_counter % 20 == 0) {
+            console.log(frame_counter);
+          }
+    
+          pending_outputs++;
+          const insert_keyframe = (frame_counter % 150) == 0;
+          encoder.encode(value, { keyFrame: insert_keyframe });
+        }
+        value.close();
+        frameReader.read().then(processFrame);
+      });
+    }
+    captureAndEncode((chunk) => {
+      sendMessage(chunk, false);
+    });
+    
+  }, [webcamRef, setCapturing]);
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    setCapturing(false);
+  }, [webcamRef, setCapturing]);
+
   return (
     <div>
+      <Webcam audio={false} ref={webcamRef} />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
       <button
         onClick={handleClickChangeSocketUrl}
       >
