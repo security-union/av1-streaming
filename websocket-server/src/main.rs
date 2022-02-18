b7c04a3d867dce5251534d4c411963c576424762
Griffin Obeid <griffobeid@gmail.com>
2022-02-18 00:21:51 -0500

    Using warp for websockets

--- websocket-server/src/main.rs
+++ websocket-server/src/main.rs
@@ -1,123 +1,32 @@
-use std::time::{Duration, Instant};
+use futures_util::{StreamExt, SinkExt};
+use warp::{Filter, ws::{WebSocket, Message}};
 
-use actix::prelude::*;
-use actix_files as fs;
-use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws;
-
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-const VIDEO_FPS: Duration = Duration::from_millis(1000);
-
-/// do websocket handshake and start `MyWebSocket` actor
-async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
-    let res = ws::start(MyWebSocket::new(), &r, stream);
-    println!("{:?}", res);
-    res
-}
-
-/// websocket connection is long running connection, it easier
-/// to handle with an actor
-struct MyWebSocket {
-    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    /// otherwise we drop connection.
-    hb: Instant,
-    nc: nats::Connection,
-}
-
-impl Actor for MyWebSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    /// Method is called on actor start. We start the heartbeat process here.
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.start_heartbeat(ctx);
-        self.start_video_stream(ctx);
-    }
-}
-
-/// Handler for `ws::Message`
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        // process websocket messages
-        println!("WS: {:?}", msg);
-        match msg {
-            Ok(ws::Message::Ping(msg)) => {
-                self.hb = Instant::now();
-                ctx.pong(&msg);
-            }
-            Ok(ws::Message::Pong(_)) => {
-                self.hb = Instant::now();
-            }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => {
-                ctx.close(reason);
-                ctx.stop();
-            }
-            _ => ctx.stop(),
-        }
-    }
-}
-
-impl MyWebSocket {
-    fn new() -> Self {
-        Self {
-            hb: Instant::now(),
-            nc: nats::connect("nats:4222").unwrap(),
-        }
-    }
-
-    /// helper method that sends ping to client every second.
-    ///
-    /// also this method checks heartbeats from client
-    fn start_heartbeat(&self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
-            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
-                // heartbeat timed out
-                println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
-                ctx.stop();
-
-                // don't try to send a ping
-                return;
-            }
-
-            ctx.ping(b"");
+#[tokio::main]
+async fn main() {
+    let routes = warp::path("ws")
+        // The `ws()` filter will prepare the Websocket handshake.
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            // And then our closure will be called when it completes...
+            ws.on_upgrade(|ws| client_connection(ws))
         });
-    }
 
-    fn start_video_stream(&self, ctx: &mut <Self as Actor>::Context) {
-        let subscription = self.nc.subscribe("video.1").unwrap();
-        ctx.run_interval(VIDEO_FPS, move |_, ctx| match subscription.next() {
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+}
+
+pub async fn client_connection(ws: WebSocket) {
+    println!("establishing client connection... {:?}", ws);
+    let (mut client_ws_sender, _client_ws_rcv) = ws.split();
+    // let (tx, rx) = mpsc::unbounded_channel();
+    let nc = async_nats::connect("nats:4222").await.unwrap();
+    let sub = nc.subscribe("video.1").await.unwrap();
+    loop {
+        match sub.next().await {
             Some(m) => {
                 println!("Forwarding video message");
-                ctx.binary(m.data);
-            }
-            None => {
-                println!("No video frame to send");
-            }
-        });
-    }
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            // websocket route
-            .service(web::resource("/ws").route(web::get().to(ws_index)))
-            // static files
-            .service(fs::Files::new("/", "static/").index_file("index.html"))
-    })
-    // start http server on 127.0.0.1:8080
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+                client_ws_sender.send(Message::binary(m.data)).await.unwrap();
+            },
+            None => {}
+        };
+    };
 }
