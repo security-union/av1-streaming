e3757bcdfcc721f0db096826a39f134606d4b686
Griffin Obeid <griffobeid@gmail.com>
2022-02-20 20:13:51 -0500

    Testing on raspberry pi

--- build-raspy.sh
+++ build-raspy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
+<<<<<<< HEAD
 PI_IP=192.168.7.116
 TARGET=armv7-unknown-linux-gnueabihf # Pi 2/3/4
 #TARGET=arm-unknown-linux-gnueabihf # Pi 0/1
+=======
+USER=ubuntu
+PI_IP=192.168.7.233
+TARGET=aarch64-unknown-linux-gnu
+#TARGET=armv7-unknown-linux-gnueabihf
+#TARGET=arm-unknown-linux-gnueabihf
+>>>>>>> 0fa9752 (Testing on raspberry pi)
 
 sudo apt update
 sudo apt install -y libclang-dev libv4l-dev
@@ -12,6 +20,11 @@ export NATS_URL=localhost:4222
 cargo build --release --target $TARGET
 
 # upload binary
+<<<<<<< HEAD
 ssh-copy-id pi@$PI_IP
 scp -r ./target/$TARGET/release/video-streaming pi@$PI_IP:/tmp/
 scp -r ./target/$TARGET/release/websocket-server pi@$PI_IP:/tmp/
+=======
+ssh-copy-id $USER@$PI_IP
+scp -r ./target/$TARGET/release/video-streaming $USER@$PI_IP:/tmp/
+>>>>>>> 0fa9752 (Testing on raspberry pi)
