c58b172d5290f03fa20c3c71fc9cc00f8e8f1c39
Griffin Obeid <griffobeid@gmail.com>
2022-03-21 20:24:45 -0400

    Adding some code and a network model

--- object-detection/video-streaming/src/main.rs
+++ object-detection/video-streaming/src/main.rs
@@ -6,15 +6,23 @@ use base64::encode;
 use bus::{Bus, BusReader};
 use futures_util::{SinkExt, StreamExt};
 use image::codecs;
-use image::ColorType;
 use image::ImageBuffer;
 use image::Rgb;
+use ndarray::prelude::*;
 use nokhwa::{Camera, CameraFormat, CaptureAPIBackend, FrameFormat};
 use rav1e::prelude::ChromaSampling;
 use rav1e::*;
 use rav1e::{config::SpeedSettings, prelude::FrameType};
 use serde::{Deserialize, Serialize};
 use serde_json;
+use tensorflow::Graph;
+use tensorflow::ImportGraphDefOptions;
+use tensorflow::Operation;
+use tensorflow::Session;
+use tensorflow::SessionOptions;
+use tensorflow::SessionRunArgs;
+use tensorflow::Tensor;
+use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Mutex};
@@ -343,4 +351,142 @@ pub async fn client_connection(
 
 pub fn since_the_epoch() -> Duration {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap()
+}
+
+pub struct ObjectDetector {
+    graph: Graph,
+    session: Session,
+    image: Box<dyn DetectionImage + Send>
+}
+
+impl ObjectDetector {
+    ///Initialize the tensorflow session with the open images ssd mobilenet v2
+    pub fn new() -> Self {
+        let mut graph = Graph::new();
+        let proto = include_bytes!("../models/openimages_v4_ssd_mobilenet_v2_1/model.pb");
+        graph.import_graph_def(proto, &ImportGraphDefOptions::new()).unwrap();
+        let session = Session::new(&SessionOptions::new(), &graph).unwrap();
+        ObjectDetector {
+            graph,
+            session,
+            image: Box::new(GenericImage::default())
+        }
+    }
+
+    ///Pass in the input image to be inferenced on. Input must implement the DetectionImage trait
+    pub fn input<I: DetectionImage + 'static>(&mut self, image: I)
+    where
+        I: std::marker::Send,
+    {
+        self.image = Box::new(image);
+    }
+
+    fn input_transform(&self) -> Result<(Operation, Tensor<u8>), Box<dyn std::error::Error>> {
+        let image_dimension = self.image.dimension();
+        let image_array = Array::from_shape_vec(
+            (
+                image_dimension.height as usize,
+                image_dimension.width as usize,
+                3,
+            ),
+            self.image.pixel_buffer().to_vec(),
+        )?;
+        let image_array_expanded = image_array.insert_axis(Axis(0));
+
+        let image_tensor_op = self.graph.operation_by_name_required("image_tensor")?;
+        let input_image_tensor = Tensor::new(&[
+            1,
+            u64::from(image_dimension.height),
+            u64::from(image_dimension.width),
+            3,
+        ])
+        .with_values(image_array_expanded.as_slice().unwrap())?;
+
+        Ok((image_tensor_op, input_image_tensor))
+    }
+
+    ///Run the inference on the inputted image transforming the image to the shape of the image input tensor,
+    ///performing the inferencing, and mapping the output tensors into the returned HashMap.
+    pub fn run(&mut self) -> Result<HashMap<&str, Tensor<f32>>, Box<dyn std::error::Error>> {
+        let (image_tensor_op, input_image_tensor) = self.input_transform()?;
+
+        let mut session_args = SessionRunArgs::new();
+        session_args.add_feed(&image_tensor_op, 0, &input_image_tensor);
+
+        let num_detections = self.graph.operation_by_name_required("num_detections")?;
+        let num_detections_token = session_args.request_fetch(&num_detections, 0);
+
+        let classes = self.graph.operation_by_name_required("detection_classes")?;
+        let classes_token = session_args.request_fetch(&classes, 0);
+
+        let boxes = self.graph.operation_by_name_required("detection_boxes")?;
+        let boxes_token = session_args.request_fetch(&boxes, 0);
+
+        let scores = self.graph.operation_by_name_required("detection_scores")?;
+        let scores_token = session_args.request_fetch(&scores, 0);
+
+        self.session.run(&mut session_args)?;
+
+        let num_detections_tensor = session_args.fetch::<f32>(num_detections_token)?;
+        let classes_tensor = session_args.fetch::<f32>(classes_token)?;
+        let boxes_tensor = session_args.fetch::<f32>(boxes_token)?;
+        let scores_tensor = session_args.fetch::<f32>(scores_token)?;
+
+        let mut tensor_map = HashMap::new();
+        tensor_map.insert("num_detections", num_detections_tensor);
+        tensor_map.insert("detection_classes", classes_tensor);
+        tensor_map.insert("detection_boxes", boxes_tensor);
+        tensor_map.insert("detection_scores", scores_tensor);
+
+        Ok(tensor_map)
+    }
+}
+
+use image::GenericImageView;
+
+#[derive(Default, Clone)]
+pub struct GenericImage {
+    image_dimension: ImageDimension,
+    pixel_buffer: Vec<u8>,
+}
+
+impl GenericImage {
+    pub fn new(width: u32, height: u32, pixel_buffer: Vec<u8>) -> Self {
+        GenericImage {
+            image_dimension: ImageDimension { width, height },
+            pixel_buffer,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct ImageDimension {
+    pub width: u32,
+    pub height: u32,
+}
+
+pub trait DetectionImage {
+    fn dimension(&self) -> ImageDimension;
+    fn pixel_buffer(&self) -> Vec<u8>;
+}
+
+impl DetectionImage for GenericImage {
+    fn dimension(&self) -> ImageDimension {
+        self.image_dimension
+    }
+
+    fn pixel_buffer(&self) -> Vec<u8> {
+        self.pixel_buffer.to_vec()
+    }
+}
+
+impl DetectionImage for image::DynamicImage {
+    fn dimension(&self) -> ImageDimension {
+        let (width, height) = self.dimensions();
+        ImageDimension { width, height }
+    }
+
+    fn pixel_buffer(&self) -> Vec<u8> {
+        self.as_bytes().to_vec()
+    }
 }
